147adc07dcbdba79f50af9d53a7ca988902f556a
Tamwoodsun <ws_tam@yahoo.com.hk>
2024-08-31 16:11:32 +0800

    Step 3 setup templates fold

--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>Hello World</h1")
+    return render(request,'pages/index.html')
+
+
+def about(request):
+    return render(request,'pages/about.html')
