b3a0eb4f5880933886e0276c585a115ffc7f9eb5
Tamwoodsun <ws_tam@yahoo.com.hk>
2024-09-03 15:00:54 +0800

    Step 7 : added listings

--- bcre/settings.py
+++ bcre/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pages.apps.PagesConfig'
+    'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'realtors.apps.RealtorsConfig',
 ]
 
 MIDDLEWARE = [
--- bcre/urls.py
+++ bcre/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('',include("pages.urls")),
+    path('',include('pages.urls')),
+    path('listings/', include('listings.urls')),
     path('admin/', admin.site.urls),
 ]
