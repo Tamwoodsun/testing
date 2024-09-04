2e3b07b104068d0d623f05665279f8bfb66f457f
Tamwoodsun <ws_tam@yahoo.com.hk>
2024-09-04 17:48:33 +0800

    Step 9 : create admin page

--- listings/models.py
+++ listings/models.py
@@ -19,12 +19,12 @@ class Listing(models.Model):
     is_published = models.BooleanField(default=True)
     list_date = models.TimeField(auto_now_add=True)
     photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/, blank=True')
-    photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/, blank=True')
-    photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/, blank=True')
-    photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/, blank=True')
-    photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/, blank=True')
-    photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/, blank=True')
+    photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     
     def __str__(self):
         return self.title
