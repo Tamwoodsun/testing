44200f3afceb665e9fa3db50ae227139c3d26fe4
Tamwoodsun <ws_tam@yahoo.com.hk>
2024-09-04 12:43:41 +0800

    Step 8 : create model files

--- bcre/settings.py
+++ bcre/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'bcre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bcre',
+        'USER': 'dbadmin',
+        'PASSWORD': '473261',
+        'HOST': 'localhost',
     }
 }
 
--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from datetime import datetime
+from realtors.models import Realtor
 
 # Create your models here.
+class Listing(models.Model):
+    realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    street = models.CharField(max_length=200)
+    district = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    price = models.IntegerField()
+    bedrooms = models.IntegerField()
+    bathrooms = models.DecimalField(max_digits=2, decimal_places=1)
+    clubhouse = models.IntegerField()
+    sqft = models.IntegerField()
+    estate_size = models.FloatField(default=0.0)
+    is_published = models.BooleanField(default=True)
+    list_date = models.TimeField(auto_now_add=True)
+    photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/, blank=True')
+    photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/, blank=True')
+    photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/, blank=True')
+    photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/, blank=True')
+    photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/, blank=True')
+    photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/, blank=True')
+    
+    def __str__(self):
+        return self.title
+    
