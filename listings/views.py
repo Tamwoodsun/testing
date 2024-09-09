55526365e389ad4ef65311c6a6d15dca4a12a4d0
Tamwoodsun <ws_tam@yahoo.com.hk>
2024-09-09 12:50:01 +0800

    Step 11: listings.html

--- listings/views.py
+++ listings/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from .models import Listing
+from django.core.paginator import EmptyPage,PageNotAnInteger, Paginator
 
 # Create your views here.
 def index(request):
     # @ get all data from listing database
-    listings = Listing.objects.all()
+    # listings = Listing.objects.all()#
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    paginator = Paginator(listings, 3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
     # @ pass database records into listings context
-    context = {'listings':listings}
+    context = {'listings': paged_listings}
     return render(request,'listings/listings.html', context)
 
 def listing(request, listing_id):
