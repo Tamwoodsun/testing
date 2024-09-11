258f164d52a041a13645e0caa03f5b05cc17a434
Tamwoodsun <ws_tam@yahoo.com.hk>
2024-09-11 10:20:37 +0800

    Step 14: added values

--- listings/models.py
+++ listings/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from datetime import datetime
 from realtors.models import Realtor
+from listings.choices import district_choices
 
 # Create your models here.
 class Listing(models.Model):
@@ -8,7 +9,7 @@ class Listing(models.Model):
     title = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     street = models.CharField(max_length=200)
-    district = models.CharField(max_length=50)
+    district = models.CharField(max_length=50, choices=district_choices.items())
     description = models.TextField(blank=True)
     price = models.IntegerField()
     bedrooms = models.IntegerField()
--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Listing
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from listings.choices import price_choices, bedroom_choices,district_choices
 
 # Create your views here.
 def index(request):
@@ -20,4 +21,44 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request,'listings/search.html')
+    queryset_list = Listing.objects.order_by('-list_date')
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(
+                description__icontains=keywords
+            )
+        if 'title' in request.GET:
+            title = request.GET['title']
+            if title:
+                queryset_list = queryset_list.filter(
+                   title__icontains=title 
+                )
+        if 'district' in request.GET:
+            district = request.GET['district']
+            if district:
+                queryset_list = queryset_list.filter(
+                   district__iexact=district
+                )
+        if 'price' in request.GET:
+            price = request.GET['price']
+            if price:
+                queryset_list = queryset_list.filter(
+                   price__lte=price
+                )
+        if 'bedrooms' in request.GET:
+            bedrooms = request.GET['bedrooms']
+            if bedrooms:
+                queryset_list = queryset_list.filter(
+                   bedrooms__lte=bedrooms
+                )       
+        
+    context ={
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices,
+        'district_choices': district_choices,
+        'listings': queryset_list,
+        'values': request.GET
+    }
+    
+    return render(request,'listings/search.html', context)
