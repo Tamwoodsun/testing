d609b1351aa5f8e9afe230fc7d81c4584d6c95de
Tamwoodsun <ws_tam@yahoo.com.hk>
2024-09-12 11:11:15 +0800

    Step 15a: register and logout

--- accounts/views.py
+++ accounts/views.py
@@ -47,6 +47,9 @@ def login(request):
         return render(request, 'accounts/login.html')
 
 def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request, 'You are now logged out')
     return redirect("index")
 
 def dashboard(request):
